9842bdda1fb805fe2bccc50c71ba5141eaa0b5bc
Tafita Hery <tafitahery@gmail.com>
2022-09-09 17:41:04 +0300

    Creating User Router

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -16,6 +18,9 @@ mongoose
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
   console.log('Backend server is running in port 8800!');

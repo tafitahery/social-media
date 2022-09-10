cba11acac4432d5d8b6b1d6ea81f9592912293b8
Tafita Hery <tafitahery@gmail.com>
2022-09-10 16:06:09 +0300

    Follow and Unfollow User

--- node-rest-api/models/User.js
+++ node-rest-api/models/User.js
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema(
     profilePicture: { type: String, default: '' },
     coverPicture: { type: String, default: '' },
     followers: { type: Array, default: [] },
-    following: { type: Array, default: [] },
+    followings: { type: Array, default: [] },
     isAdmin: { type: Boolean, default: false },
     desc: { type: String, max: 50 },
     city: { type: String, max: 50 },

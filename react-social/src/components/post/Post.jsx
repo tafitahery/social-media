67b5bd73c33a57448c3eaab73181f54adf02bcbd
Tafita Hery <tafitahery@gmail.com>
2022-09-11 16:08:44 +0300

    Feed (Share, Post Component)

---
+++ react-social/src/components/post/Post.jsx
@@ -0,0 +1,39 @@
+import './post.css';
+import { MoreVert } from '@mui/icons-material';
+
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              className="postProfileImg"
+              src="/assets/person/1.jpeg"
+              alt=""
+            />
+            <span className="postUsername">Jane Doe</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">Hey! its my first post :)</span>
+          <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="/assets/like.png" alt="" />
+            <img className="likeIcon" src="/assets/heart.png" alt="" />
+            <span className="postLikeCounter">32 peoples like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">9 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

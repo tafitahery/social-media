61f1812266c6fbc751a61d6e8a9b76f069ee133b
Tafita Hery <tafitahery@gmail.com>
2022-09-11 10:03:43 +0300

    Body Component

--- react-social/src/pages/home/Home.jsx
+++ react-social/src/pages/home/Home.jsx
@@ -1,8 +1,20 @@
 import Topbar from '../../components/topBar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Feed from '../../components/feed/Feed';
+import Rightbar from '../../components/rightbar/Rightbar';
 import './home.css';
 
 function Home() {
-  return <Topbar />;
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
 }
 
 export default Home;

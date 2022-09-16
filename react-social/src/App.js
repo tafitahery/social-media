a7d72a67bc25148323f7dca118acaded96ea8098
Tafita Hery <tafitahery@gmail.com>
2022-09-16 17:40:59 +0300

    Login system with context API

--- react-social/src/App.js
+++ react-social/src/App.js
@@ -1,16 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import Profile from './pages/profile/Profile';
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
 
 function App() {
+  const { user } = useContext(AuthContext);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={user ? <Home /> : <Register />} />
+        <Route
+          path="/login"
+          element={user ? <Navigate replace to="/" /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={user ? <Navigate replace to="/" /> : <Register />}
+        />
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </BrowserRouter>
--- react-social/src/pages/login/Login.jsx
+++ react-social/src/pages/login/Login.jsx
@@ -1,14 +1,23 @@
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
+import { CircularProgress } from '@mui/material';
+import { loginCall } from '../../apiCalls';
+import { AuthContext } from '../../context/AuthContext';
 import './login.css';
 
 function Login() {
   const email = useRef();
   const password = useRef();
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // alert(email.current.value);
+    loginCall(
+      { email: email.current.value, password: password.current.value },
+      dispatch
+    );
   };
+
+  console.log(user);
 
   return (
     <div className="login">
@@ -36,10 +45,20 @@ function Login() {
               className="loginInput"
               ref={password}
             />
-            <button className="loginButton">Log In</button>
+            <button className="loginButton" type="submit" disabled={isFetching}>
+              {isFetching ? (
+                <CircularProgress color="inherit" size="20px" />
+              ) : (
+                'Log In'
+              )}
+            </button>
             <span className="loginForgot">Forgot Password?</span>
-            <button className="loginRegisterButton">
-              Create a New Account
+            <button className="loginRegisterButton" type="button">
+              {isFetching ? (
+                <CircularProgress color="inherit" size="20px" />
+              ) : (
+                'Create a New Account'
+              )}
             </button>
           </form>
         </div>
